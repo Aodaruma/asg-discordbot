29e183333132e0f28b7f44a5ecb9bbca0374bc3a
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2023-10-01 00:15:13 +0900

    disable debugging

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -270,10 +270,10 @@ class ScheduleCog(commands.Cog):
             self.schedule_range = schedule_range
         self.website_url = website_url
         self.collect_start_date = datetime.now()
-        # self.collect_end_date = self.collect_start_date + timedelta(
-        #     days=self.schedule_collect_range
-        # )
-        self.collect_end_date = self.collect_start_date + timedelta(minutes=3)
+        self.collect_end_date = (self.collect_start_date + timedelta(
+            days=self.schedule_collect_range
+        )).replace(hour=0, minute=0, second=0)
+        # self.collect_end_date = self.collect_start_date + timedelta(minutes=3)
         author_text = f"{self.bot.ASG_NAME} 第{event_number}回"  # type: ignore
 
         # -------------------- generate schedule message and reactions for voting --------------------
