d8e30b279b78818fe25df36b7e2b4e13d8a2ffa4
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2023-10-01 00:03:23 +0900

    some updates here

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1,55 +1,81 @@
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Callable
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
 from dateutil.tz import gettz
+from dateutil.parser import parse, ParserError
 import emoji
 from num2words import num2words
+import jpholiday
 
 
 class ScheduleCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.reaction_message = None
-        self.schedule_range = 60
-        self.scrapbox_url = None
+        self.schedule_range: int = 60
+        self.start_date = None
+        self.end_date = None
+        self.website_url = None
         self.collecting = False
         self.reaction_emojis = []
         self.schedule_collect_range = 7
         self.time_range: Tuple[int, int] = (21, 23)
         self.collect_start_date: Optional[datetime] = None
         self.collect_end_date: Optional[datetime] = None
+        self.FILTER_TYPE = {
+            "all": lambda x: True,
+            "weekday": lambda x: x.weekday() < 5,
+            "weekend": lambda x: x.weekday() >= 5,
+            "holydays": lambda x: x.weekday() >= 5 or jpholiday.is_holiday(x)
+        }
 
         self.reaction_emojis += [
             emoji.emojize(f":{num2words(i+1)}:", language="alias") for i in range(9)
         ]
+        # self.reaction_emojis += [
+        #     emoji.emojize(f":regional_indicator_{chr(i+97)}:", language="alias")
+        #     for i in range(26)
+        # ]
+        #### emoji lib does not support regional indicator emojis, so use chr() instead
+        regioanl_indicator_a = u"\U0001F1E6"
         self.reaction_emojis += [
-            emoji.emojize(f":regional_indicator_{chr(i+97)}:", language="alias")
-            for i in range(26)
+            chr(ord(regioanl_indicator_a) + i) for i in range(26)
         ]
 
+        for v in self.reaction_emojis:
+            if v == "":
+                self.reaction_emojis.remove(v)
+        print("collected reaction_emojis successfully:", self.reaction_emojis)
+
     def generate_schedule_dates(
-        self, schedule_range: Optional[int] = None, timezone: str = "Asia/Tokyo"
+        self, start_date: Optional[datetime] = None, end_date: Optional[datetime]=None, schedule_range: Optional[int] = None, filter: Callable[[datetime], bool] = lambda x: True, timezone: str = "Asia/Tokyo"
     ) -> List[datetime]:
         """
-        return list of schedule dates (saturday only)
+        return list of schedule dates from start_date to end_date.
+        if end_date is None, return list of schedule dates from start_date to start_date + schedule_range.
+        :param start_date: start date
+        :param end_date: end date
         :param schedule_range: range of schedule
         :param timezone: timezone
         :return: list of schedule dates
         """
-        if schedule_range is None:
-            schedule_range = self.schedule_range
+        if start_date is None:
+            start_date = datetime.now()
+        start_date = start_date.astimezone(gettz(timezone)).replace(hour=0, minute=0, second=0)
 
-        now = (
-            datetime.now()
-            .astimezone(gettz(timezone))
-            .replace(hour=0, minute=0, second=0)
-        )
+        if end_date is None:
+            if schedule_range is None:
+                schedule_range = self.schedule_range
+        else:
+            end_date = end_date.astimezone(gettz(timezone)).replace(hour=0, minute=0, second=0)
+            schedule_range = (end_date - start_date).days + 1
+
         dates: List[datetime] = []
-        for i in range(self.schedule_range):
-            day = now + timedelta(days=i)
-            if day.weekday() == 5:
+        for i in range(schedule_range):
+            day = start_date + timedelta(days=i)
+            if filter(day):
                 dates.append(day)
         return dates
 
@@ -160,16 +186,19 @@ class ScheduleCog(commands.Cog):
         self,
         interaction: discord.Interaction,
         event_number: int,
-        schedule_range: int = 60,
+        start_date: Optional[str] = None,
+        end_date: Optional[str] = None,
+        schedule_range: Optional[int] = None,
         timezone: str = "Asia/Tokyo",
-        scrapbox_url: Optional[str] = None,
+        filter_type: str = "all",
+        website_url: Optional[str] = None,
     ):
         """
         generate schedule
         :param interaction: interaction
         :param event_number: number of event
         :param schedule_range: range of schedule
-        :param scrapbox_url: scrapbox url
+        :param website_url: website url of event
         :return: None
         """
         # -------------------- checking  --------------------
@@ -191,18 +220,87 @@ class ScheduleCog(commands.Cog):
             await interaction.response.send_message(embed=res_embed, ephemeral=True)
             return
 
+        if schedule_range is not None and end_date is not None:
+            res_embed = self.generate_embed(
+                title="エラー",
+                description="schedule_rangeとend_dateは同時に指定できません。どちらか片方のみを指定できます。",
+                color=discord.Color.red(),
+            )
+            await interaction.response.send_message(embed=res_embed, ephemeral=True)
+            return
+        
+        if filter_type not in self.FILTER_TYPE:
+            res_embed = self.generate_embed(
+                title="エラー",
+                description="filter_typeは以下のいずれかを指定してください。\n" + "\n".join(self.FILTER_TYPE.keys()),
+                color=discord.Color.red(),
+            )
+            await interaction.response.send_message(embed=res_embed, ephemeral=True)
+            return
+        
+        start_datetime: Optional[datetime] = None
+        end_datetime: Optional[datetime] = None
+        if start_date is not None:
+            try:
+                start_datetime = parse(timestr=start_date)
+            except ParserError as e:
+                res_embed = self.generate_embed(
+                    title="エラー",
+                    description="start_dateの形式が不正です。",
+                    color=discord.Color.red(),
+                )
+                await interaction.response.send_message(embed=res_embed, ephemeral=True)
+                return
+            
+        if end_date is not None:
+            try:
+                end_datetime = parse(timestr=end_date)
+            except ParserError as e:
+                res_embed = self.generate_embed(
+                    title="エラー",
+                    description="end_dateの形式が不正です。",
+                    color=discord.Color.red(),
+                )
+                await interaction.response.send_message(embed=res_embed, ephemeral=True)
+                return
+
+
         # -------------------- initialize variables --------------------
-        self.schedule_range = schedule_range
-        self.scrapbox_url = scrapbox_url
-        self.collecting = True
+        if schedule_range is not None:
+            self.schedule_range = schedule_range
+        self.website_url = website_url
         self.collect_start_date = datetime.now()
-        self.collect_end_date = self.collect_start_date + timedelta(
-            days=self.schedule_collect_range
-        )
+        # self.collect_end_date = self.collect_start_date + timedelta(
+        #     days=self.schedule_collect_range
+        # )
+        self.collect_end_date = self.collect_start_date + timedelta(minutes=3)
         author_text = f"{self.bot.ASG_NAME} 第{event_number}回"  # type: ignore
 
         # -------------------- generate schedule message and reactions for voting --------------------
-        dates = self.generate_schedule_dates()
+        dates = self.generate_schedule_dates(
+            start_date=start_datetime,
+            end_date=end_datetime,
+            schedule_range=schedule_range,
+            filter=self.FILTER_TYPE[filter_type],
+            timezone=timezone,
+        )
+        if len(dates) == 0:
+            res_embed = self.generate_embed(
+                title="エラー",
+                description="指定された条件に該当する日程が存在しませんでした。",
+                color=discord.Color.red(),
+            )
+            await interaction.response.send_message(embed=res_embed, ephemeral=True)
+            return
+        elif len(dates) > len(self.reaction_emojis):
+            res_embed = self.generate_embed(
+                title="エラー",
+                description=f"条件に一致する日程が、つけることができる絵文字数より多く存在します。{len(self.reaction_emojis)}個以下にしてください。",
+                color=discord.Color.red(),
+            )
+            await interaction.response.send_message(embed=res_embed, ephemeral=True)
+            return
+
         res_embed = self.generate_embed(
             title="以下のリアクションからスケジュールを選択してください。", author_text=author_text
         )
@@ -219,24 +317,20 @@ class ScheduleCog(commands.Cog):
             name="時間",
             value=f"{self.time_range[0]}:00 - {self.time_range[1]}:00",
         )
+        res_embed.add_field(
+            name="投票期間",
+            value=f"{self.collect_start_date.strftime('%m/%d %H:%M')} 〜 {self.collect_end_date.strftime('%m/%d %H:%M')}",
+        )
         await interaction.response.send_message(embed=res_embed, ephemeral=False)
         self.reaction_message = await interaction.original_response()
         for i in range(len(dates)):
             await self.reaction_message.add_reaction(self.reaction_emojis[i])
 
         # -------------------- wait until voting end --------------------
+        self.collecting = True
         await self.change_presence(is_collecting=True)
         await discord.utils.sleep_until(
-            # datetime(
-            #     self.collect_end_date.year,
-            #     self.collect_end_date.month,
-            #     self.collect_end_date.day,
-            #     0,
-            #     0,
-            #     0,
-            # )
-            datetime.now()
-            + timedelta(minutes=1)
+            self.collect_end_date
         )
 
         # -------------------- voting end --------------------
@@ -256,14 +350,14 @@ class ScheduleCog(commands.Cog):
         # -------------------- display result --------------------
         try:
             max_reaction_date_index: int = reaction_result.index(max(reaction_result))
-        except ValueError:
+        except ValueError as e:
             ans_embed = self.generate_embed(
                 title="エラー",
-                description="内部エラーによりスケジュールの集計に失敗しました。\nお手数をおかけしますが、手動で集計を行ってください。",
+                description="内部エラーによりスケジュールの集計に失敗しました。\nお手数をおかけしますが、手動で集計を行ってください。（エラーは開発者に通知されています）",
                 color=discord.Color.red(),
             )
             await interaction.followup.send(embed=ans_embed, ephemeral=False)
-            return
+            raise e
 
         max_reaction_date: datetime = dates[max_reaction_date_index]
         ans_embed = self.generate_embed(
@@ -288,7 +382,7 @@ class ScheduleCog(commands.Cog):
             name=f"{self.bot.ASG_NAME} 第{event_number}回",  # type: ignore
             channel=interaction.guild.voice_channels[0],
             date=max_reaction_date,
-            description=f"scrapbox: {self.scrapbox_url}" if self.scrapbox_url else None,
+            description=f"website: {self.website_url}" if self.website_url else None,
             time_range=self.time_range,
         )
         ans_embed2 = self.generate_embed(
